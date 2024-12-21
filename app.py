836f9801e60f94a7d243e3577b3dbda6fc6315ff
Aishwarya Thiyagarajan <aishwarya.t438@gmail.com>
2024-12-21 21:13:10 +0000

    included few updates in connection to user/item

--- app.py
+++ app.py
@@ -151,4 +151,4 @@ def index():
     return jsonify({"message": "composite service"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000) 
